d6e36a6ab0b3814830eeeca252cbfb53108d8469
MaiSalama <imaisalama@gmail.com>
2026-02-05 13:28:19 +0200

    Adding deploy to kubernetes stage.

--- 4-CICD/Lab_23/jenkins-shared-library/vars/deployOnK8s.groovy
+++ 4-CICD/Lab_23/jenkins-shared-library/vars/deployOnK8s.groovy
@@ -3,11 +3,19 @@ def call(String appDir) {
         sh '''
           echo "Deploying application to Kubernetes..."
 
-          kubectl version --client
-          kubectl cluster-info
+            # sanity checks
+            kubectl version --client
+            kubectl cluster-info
 
-          kubectl apply -f deployment.yaml
-          kubectl rollout status deployment/jenkins-app
+            # update image tag in deployment
+            sed -i "s|image:.*|image: ${IMAGE_NAME}:${IMAGE_TAG}|g" deployment.yaml
+
+            echo "Updated deployment.yaml:"
+            grep image deployment.yaml
+
+            # apply and wait
+            kubectl apply -f deployment.yaml
+            kubectl rollout status deployment/jenkins-app --timeout=120s
         '''
     }
 }
